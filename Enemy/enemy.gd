7db9b7c3aba2621438d24d7aeef78238ef576a68
frontier <frontier@avalon-mini.lan>
2024-01-31 20:01:11 +0100

    up to lesson 78

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-
+@export var attack_range:float = 1.5
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -9,6 +9,7 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 var player: CharacterBody3D
+@onready var animation_player = $AnimationPlayer
 
 var provoked: bool = false
 var aggro_range: float = 12.0
@@ -19,7 +20,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if provoked:
 		navigation_agent_3d.target_position = player.global_position
-	
+
 
 func _physics_process(delta: float) -> void:
 	var distance = global_position.distance_to(player.global_position)
@@ -28,6 +29,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		provoked = false
 	if provoked:
+		
+		if distance <= attack_range:
+			animation_player.play("Attack")
+		
 		var next_position = navigation_agent_3d.get_next_path_position()
 		#print(next_position)
 		# Add the gravity.
@@ -35,9 +40,10 @@ func _physics_process(delta: float) -> void:
 			velocity.y -= gravity * delta
 		
 		var direction = global_position.direction_to(next_position)
-		#print(direction)
+		print(direction)
 
 		if direction:
+			look_at_target(direction)
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
 		else:
@@ -45,3 +51,11 @@ func _physics_process(delta: float) -> void:
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 		move_and_slide()
+
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	look_at(global_position + adjusted_direction, Vector3.UP, true)
+
+func attack() -> void:
+	print("enemy attack!")
